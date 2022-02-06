c21a5fb87ec7536baa032ce50433915a7ce59b57
Bryan Garrett <bgarrett.work@gmail.com>
2022-02-06 12:22:10 -0600

    Completed first Python challenge

--- challenges/04_print_comments_variables/first_python_challenge.py
+++ challenges/04_print_comments_variables/first_python_challenge.py
@@ -1,19 +1,41 @@
 print('1: Describe what is happening below by adding comments before each line')
+# creating a variable containing the string 'books'
 box_1 = 'books'
+# creating a variable containing the string 'clothes'
 box_2 = 'clothes'
+# creating a variable containing the string 'plants'
 box_3 = 'plants'
+# creating a variable containing the string 'kitchen stuff'
 box_4 = 'kitchen stuff'
 
+# printing out a string of instructions
 print('2: Print the variables box_1, box_2, box_3, box_4')
+print('box 1 contains', box_1)
+print('box 2 contains', box_2)
+print('box 3 contains', box_3)
+print('box 4 contains', box_4)
 
+# printing out a string of instructions
 print('3.1: Declare a variable with the name "address", assign it any street name you like')
+address = '1234 Easy Street'
 
+# printing out a string of instructions
 print('3.2: Print the address variable')
+print('The boxes should be deliverd to: ', address)
 
+# printing out a string of instructions
 print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again')
+box_2 = 'trinkets'
+box_4 = 'miscellaneous'
+print('box 1 contains', box_1)
+print('box 2 contains', box_2)
+print('box 3 contains', box_3)
+print('box 4 contains', box_4)
 
+# printing out a string of instructions
 print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code')
-#completion message = 'Completed the first Python challenge!
+completion_message = 'Completed the first Python challenge!'
 
+# printing out a string of instructions
 print('5.2: Turn the comment below back into regular Python code')
-#print(completion_message)
+print(completion_message)
